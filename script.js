8dbd07822552eae965facb19ae980b56e862f508
Saurabh Ahire <ahiresaurabh369@gmail.com>
2023-02-28 18:20:14 +0530

    Added all files

---
+++ script.js
@@ -0,0 +1,57 @@
+let [milliseconds , seconds , minutes , hours] = [0,0,0,0];
+let timer = document.querySelector('.timerDisplay');
+let int = null;
+
+document.getElementById('start').addEventListener('click' , ()=>{
+    if(int !== null){
+        clearInterval(int);
+    }
+    int = setInterval(displayTimer , 10)
+})
+
+document.getElementById('pause').addEventListener('click' , ()=> {
+    clearInterval(int);
+   
+})
+
+document.getElementById('reset').addEventListener('click' , ()=>{
+    clearInterval(int);
+    [milliseconds,seconds,minutes,hours] = [0,0,0,0];
+    timer.innerHTML = '00:00:00:000'
+})
+
+document.getElementById('cast').addEventListener('click' , ()=>{
+    let cast = document.querySelector('.castDisplay')
+    let arr = [];
+    arr.push(...arr , timer.innerHTML)
+    console.log(arr);
+    let listItem = document.createElement("p");
+    listItem.innerText = `CAST : ${timer.innerHTML}`;
+    cast.appendChild(listItem)
+})
+
+function displayTimer(){
+    milliseconds += 10;
+
+    if(milliseconds == 1000){
+        milliseconds = 0;
+        seconds++;
+
+        if(seconds == 60){
+            seconds = 0;
+            minutes++;
+
+            if(minutes == 60){
+                minutes = 0;
+                hours++;
+            }
+        }
+    }
+
+    let h = hours < 10 ? '0' + hours : hours;
+    let m = minutes < 10 ? "0" + minutes : minutes;
+    let s = seconds < 10 ? "0" + seconds : seconds;
+    let ms = milliseconds <10 ? "0" + milliseconds : milliseconds <100 ? "0" + milliseconds : milliseconds;
+
+    timer.innerHTML = `${h} : ${m} : ${s} : ${ms}`
+}   
